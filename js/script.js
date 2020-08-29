57ceb6a74d118f98bb8a1a369f607b6b91c22e88
Joao Porfirio <joaoprf54@gmail.com>
2020-08-29 20:22:46 -0300

    Layout inicial da Topbar e Navbar

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* Função responsável por exibir Navbar lateral em telas pequenas */
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
